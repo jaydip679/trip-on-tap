c26e4c4933bb271be5054b1cdfc8171338bdf2e9
jaydip679 <extrajaydip1@gmail.com>
2026-02-05 08:53:19 +0530

    Add tour data for website

--- js/data.js
+++ js/data.js
@@ -2,25 +2,16 @@ const tours = [
     {
         id: 1,
         title: "Goa Beach Tour",
-        location: "Goa",
-        price: 15000,
-        duration: "3 Days",
         image: "images/goa.jpeg"
     },
     {
         id: 2,
         title: "Manali Adventure Trip",
-        location: "Manali",
-        price: 22000,
-        duration: "5 Days",
         image: "images/manali.jpeg"
     },
     {
         id: 3,
         title: "Jaipur Heritage Tour",
-        location: "Jaipur",
-        price: 12000,
-        duration: "2 Days",
         image: "images/jaipur.jpeg"
     }
 ];
